746573308ba1acda80a02227d445141d1d485982
Munish <hmunish5@gmail.com>
2023-10-23 19:05:30 +0200

    Add test messages to greetings table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(:message => "Test message 1")
+Greeting.create(:message => "Test message 2")
+Greeting.create(:message => "Test message 3")
+Greeting.create(:message => "Test message 4")
+Greeting.create(:message => "Test message 5")
